82a03b3a2423c211b5e8ee5c35c1c1ff4d569bd8
Tanivia Timbo <63170681+tanivia-code@users.noreply.github.com>
2021-08-19 02:02:57 -0300

    update mentoria13082021 0h40m23s

--- src/database/migrations/20210819032731-appointments.js
+++ src/database/migrations/20210819032731-appointments.js
@@ -1,33 +1,51 @@
 'use strict';
 
 module.exports = {
-    up: async (queryInterface, Sequelize) => {
-      return queryInterface.createTable('appointments', {
-          id: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-          },
-          date: {
-            type: Sequelize.DATE,
-            allowNull: false,
-          },
-          user_id: {
-            type: Sequelize.INTEGER,
-            references: {
-              model: 'users',
-              key: 'id'
-            },
-            onUpdate: 'CASCADE',
-            onDelete: 'SET NULL',
-            allowNull: true,
-          },
-          collaborator_id: {},
-          canceled_at: {},
-          created_at: {},
-          updated_at: {},
-      })
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('appointments', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      colaborator_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      canceled_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
   },
 
   down: queryInterface => {
