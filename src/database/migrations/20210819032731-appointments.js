d913ca3adb69e3cfdcce3259e0ab78a1842cd725
Tanivia Timbo <63170681+tanivia-code@users.noreply.github.com>
2021-08-19 01:55:58 -0300

    update mentoria13082021 0h38m42s

--- src/database/migrations/20210819032731-appointments.js
+++ src/database/migrations/20210819032731-appointments.js
@@ -1,16 +1,33 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    return queryInterface.createTable('appointments', {
-      id: {},
-      date: {},
-      user_id: {},
-      collaborator_id: {},
-      canceled_at: {},
-      created_at: {},
-      updated_at: {},
-    })
+    up: async (queryInterface, Sequelize) => {
+      return queryInterface.createTable('appointments', {
+          id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+          },
+          date: {
+            type: Sequelize.DATE,
+            allowNull: false,
+          },
+          user_id: {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'users',
+              key: 'id'
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL',
+            allowNull: true,
+          },
+          collaborator_id: {},
+          canceled_at: {},
+          created_at: {},
+          updated_at: {},
+      })
   },
 
   down: queryInterface => {
