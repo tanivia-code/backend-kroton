dac824b3f17c64e616c959c8050fa8f1ff15e612
Tanivia Timbo <63170681+tanivia-code@users.noreply.github.com>
2021-08-20 02:06:12 -0300

    update

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,22 @@
+import {
+    Router
+} from 'express';
+const routes = new Router();
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import AppointmentsController from './app/controllers/AppointmentsController';
 
 import authMiddleware from './app/middleware/auth'
+
+
+routes.get('/', (req, res) => {
+    res.json({
+        message: 'Hello kroton'
+    })
+})
+
+routes.post('/users', UserController.store);
 
 routes.post('/session', SessionController.store);
 
